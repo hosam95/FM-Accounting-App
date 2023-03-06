53c1ff091b630800ab562fadafbb81b7a6bff892
hosam95 <hosamsaeid95@gmail.com>
2023-03-06 17:04:36 +0200

    reconstract home page as a navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,59 +1,104 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:fm_accounting_app/components/users%20view/users.dart';
+import 'package:fm_accounting_app/data/partition.dart';
+import 'package:fm_accounting_app/notifiers/organisation_notifier.dart';
+import 'package:fm_accounting_app/pages/profile.dart';
+import 'package:provider/provider.dart';
+
+import '../components/buss view/buss.dart';
+import '../components/drivers view/drivers.dart';
+import '../components/partitions view/partitions.dart';
 
 class HomePageWidget extends StatefulWidget {
-  const HomePageWidget({Key? key}) : super(key: key);
+  const HomePageWidget({super.key});
 
   @override
-  _HomePageWidgetState createState() => _HomePageWidgetState();
+  State<HomePageWidget> createState() => _HomePageWidgetState();
 }
 
 class _HomePageWidgetState extends State<HomePageWidget> {
-  final _unfocusNode = FocusNode();
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  @override
-  void dispose() {
-    _unfocusNode.dispose();
-    super.dispose();
-  }
+  int currentPageIndex = 2;
 
   @override
   Widget build(BuildContext context) {
+    Provider.of<Partition>(context, listen: false).set(Partition(
+        id: "",
+        oId: Provider.of<OrganisationNotifier>(context, listen: false)
+            .organisation
+            .id,
+        name: ""));
     return Scaffold(
-      key: scaffoldKey,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Home Page'),
-        centerTitle: false,
-        elevation: 2,
-      ),
-      body: SafeArea(
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  height: 100,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Home Screen'),
-                      ElevatedButton(
-                          onPressed: () => context.go("/home/profile"),
-                          child: const Text("go to profile page."))
-                    ],
-                  ),
-                ),
-              ),
-            ],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.account_box),
+            label: 'Profile',
           ),
-        ),
+          NavigationDestination(
+            icon: Icon(Icons.supervisor_account),
+            label: 'Accounts',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.house),
+            icon: Icon(Icons.house),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.directions_bus_outlined),
+            label: 'Buss',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.directions_bike),
+            label: 'Drivers',
+          ),
+        ],
       ),
+      body: <Widget>[
+        networkCheck(const ProfilePage()),
+        Container(
+          color: Color.fromARGB(255, 231, 229, 137),
+          alignment: Alignment.center,
+          child: networkCheck(const UsersView()),
+        ),
+        Container(
+          color: Color.fromARGB(255, 128, 134, 103),
+          alignment: Alignment.center,
+          child: networkCheck(const PartitionView()),
+        ),
+        networkCheck(const BussView()),
+        Container(
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child: networkCheck(const DriversView()),
+        ),
+      ][currentPageIndex],
     );
+  }
+
+  Widget networkCheck(Widget page) {
+    bool isOk = Provider.of<OrganisationNotifier>(context, listen: false)
+            .organisation
+            .id !=
+        "";
+    return isOk
+        ? page
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Network Error",
+                style: TextStyle(color: Color.fromARGB(0, 177, 9, 9)),
+              ),
+              ElevatedButton(
+                  onPressed: () => setState(() {}), child: const Text("retry")),
+            ],
+          );
   }
 }
